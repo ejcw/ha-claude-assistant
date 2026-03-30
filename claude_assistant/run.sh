23acf3d1b2e4b221220478bd4062304ece251366
ejcw <126285890+ejcw@users.noreply.github.com>
2026-03-30 10:45:40 +0200

    Create run.sh

---
+++ claude_assistant/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "RUN.SH STARTED" >&2
+
+export ANTHROPIC_API_KEY=$(bashio::config 'anthropic_api_key')
+export HA_TOKEN=$(bashio::config 'ha_token')
+export HA_URL="http://supervisor/core"
+
+cd /usr/src/app
+exec python3 -m uvicorn app:app --host 0.0.0.0 --port 8001
